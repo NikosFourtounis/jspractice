2fe9b1557efeb3844ca9fffffbcda4f64199d0e3
N Fourtounis <nickfortune@windowslive.com>
2017-12-18 14:51:21 +0200

    JSON model

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,11 +1,21 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller"
-], function (Controller) {
-   "use strict";
-   return Controller.extend("sap.ui.demo.wt.controller.App", {
-      onShowHello : function () {
-         // show a native JavaScript alert
-         alert("Hello");
-      }
-   });
-});
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+	], function (Controller, MessageToast,JSONModel) {
+		"use strict";
+		return Controller.extend("sap.ui.demo.wt.controller.App", {
+			onInit : function () {
+				var oData = {
+					recipient : {
+						name : "World"
+					}
+				};
+				var oModel = new JSONModel(oData);
+				this.getView().setModel(oModel);
+			},
+			onShowHello : function () {
+				MessageToast.show("Hello World");
+			}
+		});
+	});
