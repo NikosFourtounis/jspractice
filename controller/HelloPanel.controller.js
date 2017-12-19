33bd6dbb0a0bcb2836f5f7d5f85155bb27229a4c
N Fourtounis <nickfortune@windowslive.com>
2017-12-19 11:22:06 +0200

    fragment

--- controller/HelloPanel.controller.js
+++ controller/HelloPanel.controller.js
@@ -17,9 +17,20 @@ sap.ui.define([
          //ERROR
          var oBundle = this.getView().getModel("i18n").getResourceBundle();
          var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-         var sMsg = oBundle.getText("Bye ", [sRecipient]);
+         var sMsg = oBundle.getText("ByeMsg", [sRecipient]);
          // show message
          MessageToast.show(sMsg);
+      },
+      onOpenDialog : function () {
+         var oView = this.getView();
+         var oDialog = oView.byId("helloDialog");
+         // create dialog lazily
+         if (!oDialog) {
+            // create dialog via fragment factory
+            oDialog = sap.ui.xmlfragment(oView.getId(), "sap.ui.demo.wt.view.HelloDialog");
+            oView.addDependent(oDialog);
+         }
+         oDialog.open();
       }
    });
 });
